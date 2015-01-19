2c8648ff05dad808d3ace117329236cc84ffe987
Aleksandar Krstikj <aleksandarkrstik@gmail.com>
2015-01-19 15:45:04 -0800

    A bunch of stuff

--- ProjectARA/Assets/Scripts/GameCreationScript/CreateGrid.cs
+++ ProjectARA/Assets/Scripts/GameCreationScript/CreateGrid.cs
@@ -4,16 +4,22 @@ using System.Collections;
 public class CreateGrid : MonoBehaviour {
 
     public Transform Tile;
+    public Transform Emitter;
+
 	// Use this for initialization
 	void Start () 
     {
 	    float tileWidth = (float)Tile.renderer.bounds.size.x;
-        for (int y = 0; y < 5; y++) {
-            for (int x = 0; x < 5; x++) {
+
+        for (int y = -4; y < 5; y++) {
+            for (int x = -4; x < 5; x++) {
                 Instantiate(Tile, new Vector3(x * tileWidth, y * tileWidth, 0), Quaternion.identity);
             }
         }
- 
+
+        Emitter.GetComponent<FireLaser>().direction = FireLaser.Direction.Right;
+        Instantiate(Emitter, new Vector3(-5 * tileWidth, 0 * tileWidth, 0), Quaternion.Euler(Vector3.forward * -90f));
+
 	}
 	
 	// Update is called once per frame
